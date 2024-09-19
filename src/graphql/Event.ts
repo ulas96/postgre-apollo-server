e051947bb934d92a0745cb82f677b4bb2ca68559
ulas96 <y.ulas.yildiz@gmail.com>
2024-09-19 22:41:38 +0300

    Update Event.ts
    events query update to select distinct transaction hash

--- src/graphql/Event.ts
+++ src/graphql/Event.ts
@@ -1,7 +1,11 @@
-import { list, extendType, intArg, nonNull, objectType, stringArg } from "nexus"; 
-import { Event } from "../entities/Event";
+import { extendType, objectType, stringArg } from "nexus"; 
 import { Context } from "../types/Context";
+import { getTimestamp } from "../helpers/index";
 
+/**
+ * @title Event Type
+ * @description Represents an event in the system
+ */
 export const EventType = objectType({
     name: "Event",
     definition(t) {
@@ -15,61 +19,40 @@ export const EventType = objectType({
         t.nonNull.list.nonNull.string("parsedData"),
         t.nonNull.string("contractAddress"),
         t.nonNull.string("appName"),
-        t.nonNull.string("createdAt")
+        t.nonNull.float("timestamp")
     }
 });
 
-export const MintedTokensType = objectType({
-    name: "MintedTokens",
-    definition(t) {
-      t.string("walletAddress");  
-      t.string("totalMinted");  
-    },
-  });
 
-export const PoolDepositType = objectType({
-    name: "PoolDeposit",
-    definition(t) {
-        t.nonNull.string("walletAddress");
-        t.nonNull.string("totalDeposits");
-    }
-})
 
-export const PoolUnlockType = objectType({
-    name: "PoolUnlock",
-    definition(t) {
-        t.nonNull.string("walletAddress");
-        t.nonNull.string("totalUnlocks");
-    }
-})
-
-export const PositionType = objectType({
-  name: "Position",
-  definition(t) {
-    t.nonNull.string("walletAddress");
-    t.nonNull.string("totalDeposits");
-    t.nonNull.string("totalUnlocks");
-    t.nonNull.string("position");
-  }
-});
-
+/**
+ * @title Events Query
+ * @description Query to fetch events, optionally filtered by wallet address
+ * @param walletAddress - Optional wallet address to filter events
+ * @returns List of Event objects
+ */
 export const EventsQuery = extendType({
     type: "Query",
     definition(t) {
         t.nonNull.list.nonNull.field("events", {
             type: "Event",
             args: {
-                walletAddress: stringArg(), // Optional argument
+                walletAddress: stringArg(),
             },
             async resolve(_parent, args, context: Context, _info) {
                 const { connection } = context;
                 const { walletAddress } = args;
 
-                let query = `SELECT * FROM event`;
+                let query = `
+                    SELECT DISTINCT ON ("transactionHash") *
+                    FROM event
+                `;
 
                 if (walletAddress) {
                     query += ` WHERE "parsedData"[1] = $1 OR "parsedData"[2] = $1`;
                 }
+
+                query += ` ORDER BY "transactionHash", "blockNumber" DESC`;
 
                 try {
                     let result;
@@ -78,237 +61,28 @@ export const EventsQuery = extendType({
                     } else {
                         result = await connection.query(query);
                     }
-                    console.log('Events Query Result:', result);
-                    return result;
+
+                    // Calculate timestamp for each event
+                    const eventsWithTimestamp = await Promise.all(result.map(async (event: any) => {
+                        const timestamp = await getTimestamp(event.blockNumber);
+                        return { ...event, timestamp: timestamp.getTime() };
+                    }));
+
+                    console.log('Unique Events Query Result:', eventsWithTimestamp);
+                    return eventsWithTimestamp;
                 } catch (error) {
-                    console.error('Error executing events query:', error);
-                    throw new Error('Failed to fetch events');
+                    console.error('Error executing unique events query:', error);
+                    throw new Error('Failed to fetch unique events');
                 }
             }
         });
     }
 });
 
-export const MintedTokensQuery = extendType({
-    type: "Query",
-    definition(t) {
-      t.nonNull.list.field("mintedTokens", {  // Changed to nullable list items
-        type: "MintedTokens",
-        args: {
-            walletAddress: stringArg(), // Optional argument
-        },
-        async resolve(_parent, args, context: Context, _info) {
-            const { connection } = context;
-            const { walletAddress } = args;
 
-            let query = `
-              SELECT 
-                "parsedData"[2] as "walletAddress",
-                SUM(CAST("parsedData"[3] AS DECIMAL(65,0)))::TEXT as "totalMinted"
-              FROM event
-              WHERE 
-                "eventName" = 'Transfer' AND
-                "parsedData"[1] = '0x0000000000000000000000000000000000000000'
-            `;
 
-            // Add wallet address filter if provided
-            if (walletAddress) {
-                query += ` AND "parsedData"[2] = '${walletAddress}'`;
-            }
 
-            query += ` GROUP BY "parsedData"[2]`;
 
-            try {
-                const result = await connection.query(query);
-                console.log('Minted Tokens Query Result:', result);
-                return result;
-            } catch (error) {
-                console.error('Error executing minted tokens query:', error);
-                throw new Error('Failed to fetch minted tokens');
-            }
-        },
-      });
-    },
-  });
 
-  export const PoolDepositsQuery = extendType({
-    type: "Query",
-    definition(t) {
-        t.nonNull.list.nonNull.field("poolDeposits", {
-            type: "PoolDeposit",
-            args: {
-                walletAddress: stringArg(), // Optional argument
-            },
-            async resolve(_parent, args, context: Context, _info) {
-                const { connection } = context;
-                const { walletAddress } = args;
 
-                let query = `
-                    SELECT 
-                        "parsedData"[1] as "walletAddress",
-                        SUM(CAST("parsedData"[3] AS DECIMAL(65,0)))::TEXT as "totalDeposits"
-                    FROM event
-                    WHERE 
-                        ("parsedData"[2] = '0x934cf521743903D27e388d7E8517c636f3Cc4D54' OR
-                        "parsedData"[2] = '0x1a66208180c20cc893ac5092d0cce95994cb1ae0' OR
-                        "parsedData"[2] = '0x0363a3deBe776de575C36F524b7877dB7dd461Db') AND
-                        "parsedData"[1] != '0x0000000000000000000000000000000000000000'
-                `;
 
-                // Add wallet address filter if provided
-                if (walletAddress) {
-                    query += ` AND "parsedData"[1] = '${walletAddress}'`;
-                }
-
-                query += ` GROUP BY "parsedData"[1]`;
-
-                try {
-                    const result = await connection.query(query);
-                    console.log('Pool Deposits Query Result:', result);
-                    return result;
-                } catch (error) {
-                    console.error('Error executing pool deposits query:', error);
-                    throw new Error('Failed to fetch pool deposits');
-                }
-            }
-        })
-    }
-});
-
-export const PoolUnlocksQuery = extendType({
-    type: "Query",
-    definition(t) {
-        t.nonNull.list.nonNull.field("poolUnlocks", {
-            type: "PoolUnlock",
-            args: {
-                walletAddress: stringArg(), // Optional argument
-            },
-            async resolve(_parent, args, context: Context, _info) {
-                const { connection } = context;
-                const { walletAddress } = args;
-
-                let query = `
-                    SELECT 
-                        "parsedData"[2] as "walletAddress",
-                        SUM(CAST("parsedData"[3] AS DECIMAL(65,0)))::TEXT as "totalUnlocks"
-                    FROM event
-                    WHERE 
-                        ("parsedData"[1] = '0x934cf521743903D27e388d7E8517c636f3Cc4D54' OR
-                        "parsedData"[1] = '0x1a66208180c20cc893ac5092d0cce95994cb1ae0' OR
-                        "parsedData"[1] = '0x0363a3deBe776de575C36F524b7877dB7dd461Db') AND
-                        "parsedData"[2] != '0x0000000000000000000000000000000000000000'
-                `;
-
-                // Add wallet address filter if provided
-                if (walletAddress) {
-                    query += ` AND "parsedData"[2] = '${walletAddress}'`;
-                }
-
-                query += ` GROUP BY "parsedData"[2]`;
-
-                try {
-                    const result = await connection.query(query);
-                    console.log('Pool Unlocks Query Result:', result);
-                    return result;
-                } catch (error) {
-                    console.error('Error executing pool unlocks query:', error);
-                    throw new Error('Failed to fetch pool unlocks');
-                }
-            }
-        })
-    }
-});
-
-export const PositionsQuery = extendType({
-  type: "Query",
-  definition(t) {
-    t.nonNull.list.nonNull.field("positions", {
-      type: "Position",
-      args: {
-        walletAddress: stringArg(), // Optional argument
-      },
-      async resolve(_parent, args, context: Context, _info) {
-        const { connection } = context;
-        const { walletAddress } = args;
-
-        let query = `
-          WITH deposits AS (
-            SELECT 
-              "parsedData"[1] as "walletAddress",
-              SUM(CAST("parsedData"[3] AS DECIMAL(65,0)))::TEXT as "totalDeposits"
-            FROM event
-            WHERE 
-              ("parsedData"[2] = '0x934cf521743903D27e388d7E8517c636f3Cc4D54' OR
-              "parsedData"[2] = '0x1a66208180c20cc893ac5092d0cce95994cb1ae0' OR
-              "parsedData"[2] = '0x0363a3deBe776de575C36F524b7877dB7dd461Db') AND
-              "parsedData"[1] != '0x0000000000000000000000000000000000000000'
-            GROUP BY "parsedData"[1]
-          ),
-          unlocks AS (
-            SELECT 
-              "parsedData"[2] as "walletAddress",
-              SUM(CAST("parsedData"[3] AS DECIMAL(65,0)))::TEXT as "totalUnlocks"
-            FROM event
-            WHERE 
-              ("parsedData"[1] = '0x934cf521743903D27e388d7E8517c636f3Cc4D54' OR
-              "parsedData"[1] = '0x1a66208180c20cc893ac5092d0cce95994cb1ae0' OR
-              "parsedData"[1] = '0x0363a3deBe776de575C36F524b7877dB7dd461Db') AND
-              "parsedData"[2] != '0x0000000000000000000000000000000000000000'
-            GROUP BY "parsedData"[2]
-          )
-          SELECT 
-            COALESCE(u."walletAddress", d."walletAddress") as "walletAddress",
-            COALESCE(d."totalDeposits", '0') as "totalDeposits",
-            COALESCE(u."totalUnlocks", '0') as "totalUnlocks",
-            ((COALESCE(d."totalDeposits", '0')::DECIMAL - COALESCE(u."totalUnlocks", '0')::DECIMAL) / 1000000000000000000)::TEXT as "position"
-          FROM unlocks u
-          FULL OUTER JOIN deposits d ON u."walletAddress" = d."walletAddress"
-        `;
-
-        // Add wallet address filter if provided
-        if (walletAddress) {
-          query += ` WHERE COALESCE(d."walletAddress", u."walletAddress") = '${walletAddress}'`;
-        }
-
-        try {
-          const result = await connection.query(query);
-          console.log('Positions Query Result:', result);
-          return result;
-        } catch (error) {
-          console.error('Error executing positions query:', error);
-          throw new Error('Failed to fetch positions');
-        }
-      }
-    });
-  }
-});
-
-export const CreateEventMutation = extendType({
-    type: "Mutation",
-    definition(t) {
-        t.nonNull.field("createEvent", {
-            type: "Event",
-            args: {
-                id: nonNull(intArg()),
-                eventName: nonNull(stringArg()),
-                eventSignature: nonNull(stringArg()),
-                eventData: nonNull(stringArg()),
-                blockNumber: nonNull(intArg()),
-                transactionHash: nonNull(stringArg()),
-                logIndex: nonNull(intArg()),
-                parsedData: nonNull(list(nonNull(stringArg()))),
-                contractAddress: nonNull(stringArg()),
-                appName: nonNull(stringArg()),
-                createdAt: nonNull(stringArg())
-            },
-            resolve(_parent, args, _context, _info) {
-                const { id, eventName, eventSignature, eventData, blockNumber, transactionHash, logIndex, parsedData, contractAddress, appName, createdAt } = args;
-                
-                return Event.create({ id, eventName, eventSignature, eventData, blockNumber, transactionHash, logIndex, parsedData, contractAddress, appName, createdAt: new Date(createdAt) }).save().then(event => ({
-                    ...event,
-                    createdAt: event.createdAt.toISOString()
-                }));
-            }
-        })
-    },
-})
